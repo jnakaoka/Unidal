c1a8f594dd0f3c19ade23bd146f260bd1fc28d23
Johann <johannnakaoka_7@hotmail.com>
2026-07-13 10:09:02 +0100

    Adiciona baseline e prepara portabilidade do banco

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,6 +3,8 @@
 from .perfil import Perfil
 from .user import User
 from .projeto import Projeto
+from .cliente import Cliente
+from .obra import Obra
 from .registro_hora import RegistroHora
 #tables
 from .registro_hora import RegistroHoraEquipa
